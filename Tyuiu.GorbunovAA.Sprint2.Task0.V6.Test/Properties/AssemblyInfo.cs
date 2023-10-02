282055044237dd2a10381e436c61fcc7db19e92d
Александр Горбунов <Tyuiu.GorbunovAA@outlook.com>
2023-10-02 17:19:28 +0500

    Add project files.

---
+++ Tyuiu.GorbunovAA.Sprint2.Task0.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GorbunovAA.Sprint2.Task0.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GorbunovAA.Sprint2.Task0.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6cf4a9a6-82dd-43e5-ad8e-1fb3304bf12e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
