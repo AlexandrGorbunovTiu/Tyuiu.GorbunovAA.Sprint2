d169413ade7a3ff19691ac7336a59fe0a216e1fe
Александр Горбунов <Tyuiu.GorbunovAA@outlook.com>
2023-10-12 16:00:23 +0500

    Task7

---
+++ Tyuiu.GorbunovAA.Sprint2.Task7.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GorbunovAA.Sprint2.Task7.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GorbunovAA.Sprint2.Task7.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64125ef7-d04f-4816-ae4e-c70d962048dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
